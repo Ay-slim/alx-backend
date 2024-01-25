b3278ec920ee9ddc84e39b06ba9630b74c03a0b3
Ay-slim <ayooluwaadedipe@gmail.com>
2024-01-25 15:13:36 +0000

    Pagination

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Module to fetch data within a range"""
+
+
+def index_range(page: int, page_size: int) -> tuple[int, int]:
+    """
+    index_range - Helper function to specify index
+    @page: Int, Page to calculate index for
+    @page_size: Int, size of each page
+    Returns: Tuple containing indices
+    """
+    start_idx = (page - 1) * page_size
+    return (start_idx, start_idx + page_size)
