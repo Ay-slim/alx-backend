bfa0893dd360d4c5dcb2a12a9338c5261f0a9040
Ay-slim <ayooluwaadedipe@gmail.com>
2024-02-06 15:18:13 +0000

    Parametrize templates

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""Module to bootstrap a Flask app with babel"""
+
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, gettext
+from typing import Any
+
+
+app = Flask(__name__)
+
+
+class Config(object):
+    """Babel config"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+def get_locale() -> Any:
+    """Fetch a request's locale"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+babel.init_app(app, local_selector=get_locale)
+
+
+@app.route('/')
+def home() -> Any:
+    """Application landing page"""
+    return render_template('3-index.html',
+                           title=gettext('home_title'),
+                           body=gettext('home_header'))
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
